ec785df5e034a1d9d1e57a433dab73c5e1df03c3
Deepti Manna <www.deeptimanna786@gmail.com>
2025-12-27 20:01:17 +0530

    fixed pages and layout

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -5,6 +5,7 @@ import Header from "@/components/header";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 export default async function AdminLayout({ children }) {
   const admin = await getAdmin();
